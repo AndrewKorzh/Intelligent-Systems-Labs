80db5ed0ff1dd41d0b9e6a97e8e7b789cc623363
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2024-09-08 00:41:26 +0300

    Update lab1_extra.cpp

--- lab1/lab1_extra.cpp
+++ lab1/lab1_extra.cpp
@@ -19,22 +19,25 @@ struct Chip
 struct Node
 {
     Chip chip;
+    bool flip;
     std::deque<Chip> q;
 
-    Node() : chip(), q() {}
+    Node() : chip(), q(), flip(false) {}
 
     void print() const
     {
 
         std::cout << "Chip: ";
         chip.print();
-        std::cout << std::endl;
+        std::cout << "; ";
 
-        std::cout << "Deque of chips: ";
+        std::cout << "C: " << flip << "; ";
+
+        std::cout << " Deque: ";
         for (const auto &ch : q)
         {
-            std::cout << std::endl;
             ch.print();
+            std::cout << ", ";
         }
         std::cout << std::endl;
     }
@@ -57,10 +60,9 @@ std::deque<Chip> generateAllDominoChips()
 
     for (int top = 0; top <= 6; ++top)
     {
-        for (int down = 0; down <= 6; ++down)
+        for (int down = top; down <= 6; ++down)
         {
             chips.emplace_back(top, down);
-            chips.emplace_back(down, top);
         }
     }
 
@@ -85,24 +87,24 @@ bool checkLimitRools(const std::deque<Node> &nodes, int val)
         }
     }
 
-    // // Проверяем по вертикали
-    // for (int i = 0; i < 6; ++i)
-    // {
-    //     int sum = nodes[i].chip.top + nodes[i].chip.down + nodes[i + 6].chip.top + nodes[i + 6].chip.down + nodes[i + 12].chip.top + nodes[i + 12].chip.down;
-    //     if (sum > val)
-    //     {
-    //         return false;
-    //     }
-    // }
+    // Проверяем по вертикали
+    for (int i = 0; i < 6; ++i)
+    {
+        int sum = nodes[i].chip.top + nodes[i].chip.down + nodes[i + 6].chip.top + nodes[i + 6].chip.down + nodes[i + 12].chip.top + nodes[i + 12].chip.down;
+        if (sum > val)
+        {
+            return false;
+        }
+    }
 
-    // // Проверяем по диаг
-    // int main_diagonal = nodes[0].chip.top + nodes[1].chip.down + nodes[8].chip.top + nodes[9].chip.down + nodes[16].chip.top + nodes[17].chip.down;
-    // int side_diagonal = nodes[12].chip.down + nodes[13].chip.top + nodes[8].chip.down + nodes[9].chip.top + nodes[4].chip.down + nodes[5].chip.top;
+    // Проверяем по диаг
+    int main_diagonal = nodes[0].chip.top + nodes[1].chip.down + nodes[8].chip.top + nodes[9].chip.down + nodes[16].chip.top + nodes[17].chip.down;
+    int side_diagonal = nodes[12].chip.down + nodes[13].chip.top + nodes[8].chip.down + nodes[9].chip.top + nodes[4].chip.down + nodes[5].chip.top;
 
-    // if (main_diagonal > val || side_diagonal > val)
-    // {
-    //     return false;
-    // }
+    if (main_diagonal > val || side_diagonal > val)
+    {
+        return false;
+    }
 
     return true;
 }
@@ -152,8 +154,6 @@ void printField(const std::deque<Node> &nodes)
     std::cout << "\n\n";
     for (int i = 0; i < 3; ++i)
     {
-        int top_sum = 0;
-        int down_sum = 0;
         for (int j = i * 6; j < (i * 6 + 6); ++j)
         {
             std::cout << nodes[j].chip.top << " ";
@@ -168,9 +168,31 @@ void printField(const std::deque<Node> &nodes)
     std::cout << "\n\n";
 }
 
+// void printNodes(const std::deque<Node> &nodes)
+// {
+//     std::cout << "\n\n";
+//     for (int i = 0; i < nodes.size(); ++i)
+//     {
+//         std::cout << "[" << nodes[i].chip.top << ", " << nodes[i].chip.down << "] {" << nodes[i].q.size() << "},   ";
+//     }
+//     std::cout << "\n\n";
+// }
+
+void printNodes(const std::deque<Node> &nodes)
+{
+    std::cout << "\n\n";
+    for (int i = 0; i < nodes.size(); ++i)
+    {
+        std::cout << i + 1 << ") ";
+        nodes[i].print();
+    }
+    std::cout << "\n\n";
+}
+
 int main()
 {
     std::deque<Chip> domino_chips = generateAllDominoChips();
+    std::cout << domino_chips.size() << std::endl;
 
     std::deque<Node> nodes;
     for (int i = 0; i < 18; ++i)
@@ -180,15 +202,19 @@ int main()
 
     nodes[0].q = domino_chips;
 
+    printNodes(nodes);
+
     int index = 0;
 
-    std::cout << "Trying!" << std::endl;
-
-    int big_count = 0;
+    int count = 0;
     while (true)
     {
-        // std::cout << "q: " << nodes[index].q.size() << std::endl;
-        // printField(nodes);
+        count += 1;
+        if (count % 10000 == 0)
+        {
+            printField(nodes);
+        }
+        // printNodes(nodes);
         if (index < 0)
         {
             std::cout << "  Impossible! No result!" << std::endl;
@@ -197,9 +223,6 @@ int main()
 
         if (index == 18)
         {
-            big_count += 1;
-            std::cout << "Big check - " << big_count << std::endl;
-            // big ckeck
             if (checkRools(nodes, 13))
             {
                 std::cout << "Found!" << std::endl;
@@ -224,17 +247,86 @@ int main()
             if (index < 17)
             {
                 nodes[index + 1].q.clear();
+                std::copy(nodes[index].q.begin(), nodes[index].q.end(), std::back_inserter(nodes[index + 1].q));
 
-                nodes[index + 1].q.assign(nodes[index].q.begin(), nodes[index].q.end());
+                // nodes[index + 1].q.assign(nodes[index].q.begin(), nodes[index].q.end());
             }
             index += 1;
         }
     }
 
-    // std::cout << checkLimitRools(nodes, 0);
-
     return 0;
 }
+
+//    std::cout << nodes.size();
+// int main()
+// {
+//     std::deque<Chip> domino_chips = generateAllDominoChips();
+
+//     std::deque<Node> nodes;
+//     for (int i = 0; i < 18; ++i)
+//     {
+//         nodes.emplace_back();
+//     }
+
+//     nodes[0].q = domino_chips;
+
+//     int index = 0;
+
+//     std::cout << "Trying!" << std::endl;
+
+//     int big_count = 0;
+//     while (true)
+//     {
+//         // std::cout << "q: " << nodes[index].q.size() << std::endl;
+//         // printField(nodes);
+//         printNodes(nodes);
+//         if (index < 0)
+//         {
+//             std::cout << "  Impossible! No result!" << std::endl;
+//             break;
+//         }
+
+//         if (index == 18)
+//         {
+//             big_count += 1;
+//             std::cout << "Big check - " << big_count << std::endl;
+//             // big ckeck
+//             if (checkRools(nodes, 13))
+//             {
+//                 std::cout << "Found!" << std::endl;
+//                 break;
+//             }
+//             else
+//             {
+//                 index -= 1;
+//             }
+//         }
+//         if (nodes[index].q.empty())
+//         {
+//             nodes[index].chip.down = 0;
+//             nodes[index].chip.top = 0;
+//             index -= 1;
+//         }
+//         nodes[index].chip.down = nodes[index].q.front().down;
+//         nodes[index].chip.top = nodes[index].q.front().top;
+//         nodes[index].q.pop_front();
+//         if (checkLimitRools(nodes, 13))
+//         {
+//             if (index < 17)
+//             {
+//                 nodes[index + 1].q.clear();
+
+//                 nodes[index + 1].q.assign(nodes[index].q.begin(), nodes[index].q.end());
+//             }
+//             index += 1;
+//         }
+//     }
+
+//     // std::cout << checkLimitRools(nodes, 0);
+
+//     return 0;
+// }
 
 // for (int i = 0; i < nodes.size(); ++i)
 // {
