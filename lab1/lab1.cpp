c823b1002125783d4bd7da13734843f4c4f8675a
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2024-09-14 01:06:58 +0300

    update readme

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -291,43 +291,6 @@ std::deque<int> task4(int start_value = 2, int end_value = 10000)
     return result;
 }
 
-std::unordered_set<int> expand_left_front(std::deque<Node> &nodes, std::unordered_set<int> &passed_values, int front_begin, int front_end)
-{
-    int count = 0;
-    for (int i = front_begin; i <= front_end; i++)
-    {
-        int val = nodes[i].value + 3;
-        if (passed_values.find(val) == passed_values.end())
-        {
-            nodes.emplace_back(val, i);
-            passed_values.insert(val);
-        }
-        val = nodes[i].value * 2;
-        if (passed_values.find(val) == passed_values.end())
-        {
-            nodes.emplace_back(val, i);
-            passed_values.insert(val);
-        }
-    }
-}
-std::deque<int> task4_remastered(int start_value = 2, int end_value = 10000)
-{
-    std::unordered_set<int> left_passed_values;
-    std::unordered_set<int> right_passed_values;
-
-    std::deque<Node> left_nodes;
-    std::deque<Node> right_nodes;
-
-    left_nodes.emplace_back(start_value, -1);
-    right_nodes.emplace_back(end_value, -1);
-
-    int left_front_begin = 1;
-    int left_front_end = 1;
-
-    int right_front_begin = 1;
-    int right_front_end = 1;
-}
-
 void executeFunction(int start_value, int end_value, std::deque<int> (*function)(int, int))
 {
     auto start = std::chrono::high_resolution_clock::now();
@@ -358,3 +321,40 @@ int main()
 
     return 0;
 }
+
+// std::unordered_set<int> expand_left_front(std::deque<Node> &nodes, std::unordered_set<int> &passed_values, int front_begin, int front_end)
+// {
+//     int count = 0;
+//     for (int i = front_begin; i <= front_end; i++)
+//     {
+//         int val = nodes[i].value + 3;
+//         if (passed_values.find(val) == passed_values.end())
+//         {
+//             nodes.emplace_back(val, i);
+//             passed_values.insert(val);
+//         }
+//         val = nodes[i].value * 2;
+//         if (passed_values.find(val) == passed_values.end())
+//         {
+//             nodes.emplace_back(val, i);
+//             passed_values.insert(val);
+//         }
+//     }
+// }
+// std::deque<int> task4_remastered(int start_value = 2, int end_value = 10000)
+// {
+//     std::unordered_set<int> left_passed_values;
+//     std::unordered_set<int> right_passed_values;
+
+//     std::deque<Node> left_nodes;
+//     std::deque<Node> right_nodes;
+
+//     left_nodes.emplace_back(start_value, -1);
+//     right_nodes.emplace_back(end_value, -1);
+
+//     int left_front_begin = 1;
+//     int left_front_end = 1;
+
+//     int right_front_begin = 1;
+//     int right_front_end = 1;
+// }
