f55ca8f63efc69aeade10ed5b59314b216294d38
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2024-09-19 19:49:41 +0400

    Чистка

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -13,8 +13,8 @@
 #include <chrono>
 #include <unordered_set>
 #include <sstream>
+#include <unordered_map>
 
-int rand(void);
 struct Node
 {
     std::string condition;
@@ -33,9 +33,9 @@ int manhattan_distance4x4(const std::string &state)
     for (int i = 0; i < state.size(); ++i)
     {
         char tile = state[i];
-        if (tile != '0') // Пропускаем пустую плитку
+        if (tile != '0')
         {
-            int value = (tile <= '9') ? tile - '1' : tile - 'A' + 9; // Преобразование плитки в целое значение
+            int value = (tile <= '9') ? tile - '1' : tile - 'A' + 9;
             int current_x = i % grid_size;
             int current_y = i / grid_size;
             int target_x = value % grid_size;
@@ -51,7 +51,7 @@ struct Compare
 {
     bool operator()(const Node &n1, const Node &n2)
     {
-        return n1.g + manhattan_distance4x4(n1.condition) > n2.g + manhattan_distance4x4(n2.condition); // тут элемент с меньшим приоритетом будет наверху
+        return n1.g + manhattan_distance4x4(n1.condition) > n2.g + manhattan_distance4x4(n2.condition);
     }
 };
 
@@ -112,23 +112,6 @@ void draw_field(const std::string &str, bool er = false, std::string info = "")
     std::cout << std::endl;
 }
 
-void draw_sequence(const std::list<std::string> &seq, int sleep_time, bool er)
-{
-    for (std::string value : seq)
-    {
-        draw_field(value, er);
-        Sleep(sleep_time);
-    }
-}
-void draw_nodes_sequence(const std::deque<Node> &nodes, int sleep_time = 300, bool er = true)
-{
-    for (int i = 0; i < nodes.size(); ++i)
-    {
-        draw_field(nodes[i].condition, er);
-        Sleep(sleep_time);
-    }
-}
-
 std::string swap_positions(const std::string &s, int i1, int i2)
 {
     std::string res = s;
@@ -139,7 +122,6 @@ std::string swap_positions(const std::string &s, int i1, int i2)
 
 std::deque<Node> next_nodes(const Node &node, std::map<int, std::deque<int>> &moves)
 {
-    // Condition, last position, empty index
     std::deque<Node> nodes;
     for (int i = 0; i < moves[node.empty_index].size(); ++i)
     {
@@ -201,7 +183,7 @@ std::deque<std::string> solve_game_A_star(Node start_node, std::map<int, std::de
 {
     std::string start_condition = start_node.condition;
     std::string target_condition = "123456789ABCDEF0";
-    std::map<std::string, std::string> parent_map;
+    std::unordered_map<std::string, std::string> parent_map;
 
     std::deque<std::string> result;
 
@@ -262,11 +244,6 @@ int main()
         {14, {10, 13, 15}},
         {15, {11, 14}}};
 
-    // auto condition = "5134207896ACDEBF";
-    // auto condition = "0FEDCBA987654321";
-    // auto condition = "123456789ABCDE0F";
-    // auto condition = "123456789ABCDEF0";
-
     std::map<int, std::string> conditions{
         {-2, "123456789AFB0EDC"},
         {-1, "F2345678A0BE91CD"},
@@ -288,7 +265,12 @@ int main()
 
     };
 
-    auto condition = conditions[35];
+    // Странно - 48 считает за сек, а 45 - за 30
+
+    auto condition = conditions[58];
+
+    condition = "1234ABC567890DFE";
+
     auto node = create_start_node(condition);
     int draw_time_sleep = 300;
     bool erace = true;
@@ -296,7 +278,9 @@ int main()
     if (isSolvable4x4(node.condition))
     {
 
-        std::cout << "Can Solve" << std::endl;
+        std::cout << "Possible to Solve\n"
+                  << "manhattan_distance: " << manhattan_distance4x4(node.condition) << std::endl;
+
         auto start = std::chrono::high_resolution_clock::now();
         auto res = solve_game_A_star(node, moves);
         auto end = std::chrono::high_resolution_clock::now();
@@ -316,98 +300,6 @@ int main()
     }
     else
     {
-        std::cout << "Cant Solve" << std::endl;
+        std::cout << "Impossible to Solve" << std::endl;
     }
 }
-//
-//
-//
-//
-//
-//
-//
-///////////////////        not used    ///////////////////////
-// std::deque<Node> random_node_seq(Node node, std::map<int, std::deque<int>> &moves, int length = 10)
-// {
-//     std::deque<Node> nodes;
-//     nodes.emplace_back(node);
-
-//     int g_start = node.g;
-
-//     for (int i = 1; i < length; ++i)
-//     {
-//         auto next_steps_deque = next_steps(nodes.back(), moves);
-//         auto next_random_node = next_steps_deque[rand() % next_steps_deque.size()];
-//         nodes.emplace_back(std::get<0>(next_random_node), std::get<1>(next_random_node), std::get<2>(next_random_node), g_start + i);
-//     }
-
-//     return nodes;
-// }
-
-// std::deque<std::tuple<std::string, int, int>> next_steps(const Node &node, std::map<int, std::deque<int>> &moves)
-// {
-//     // Condition, last position, empty index
-//     std::deque<std::tuple<std::string, int, int>> ns;
-//     for (int i = 0; i < moves[node.empty_index].size(); ++i)
-//     {
-//         if (moves[node.empty_index][i] != node.last_position)
-//         {
-//             ns.emplace_back(swap_positions(node.condition, moves[node.empty_index][i], node.empty_index), node.empty_index, moves[node.empty_index][i]);
-//         }
-//     }
-//     return ns;
-// }
-///////////////////////////////////////////////////////////////////////
-
-// draw_nodes_sequence(random_node_seq(node, moves, 20), 500, true);
-//  std::deque<Node> nodes;
-
-// nodes.emplace_back("123456789ABCDEF0", 0, 14, 15);
-
-// int index = 0;
-// while (true)
-// {
-//     index += 1;
-//     auto next_steps_val = next_steps(nodes.back(), moves);
-
-//     auto next_step = next_steps_val[rand() % next_steps_val.size()];
-
-//     nodes.emplace_back(std::get<0>(next_step), 0, std::get<1>(next_step), std::get<2>(next_step)); // неправильно из кортнжа
-//     if (index > 100)
-//     {
-//         break;
-//     }
-// }
-
-// for (int i = 0; i < nodes.size(); ++i)
-// {
-//     draw_field(nodes[i].condition, true);
-//     Sleep(800);
-// }
-// auto ns = next_steps(n, moves);
-
-// for
-
-// draw_field(n.condition);
-
-// draw_field(ns[0]);
-
-// for (int i = 0; i < ns.size(); ++i)
-// {
-//     std::cout << ns[i] << std::endl;
-// }
-
-// Node n("123456789ABCDEF0", -1, 14, 15);
-
-// next_steps(n, moves);
-
-// std::string s = "123123321";
-
-// s[0] = '3';
-
-// std::cout << s << std::endl;
-// std::list<std::string> seq4 = {"123456789ABCDE0F", "123456789ABCDEF0", "123456789AB0DEFC"};
-// draw_sequence(seq4, 600, true);
-
-// std::list<std::string> seq3 = {"123456780", "123456708", "123456078", "123056478", "123506478"};
-// draw_sequence(seq3, 1, true);
